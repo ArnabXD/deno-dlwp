65e2260ad73f3d11463b2cfa2b2b0ff79ab2ac9d
Arnab Paryali <arnabxd@proton.me>
2022-08-24 00:07:08 +0530

    feat: average speed calculation over last 5 secs

--- download.ts
+++ download.ts
@@ -6,21 +6,57 @@ import formatDistance from "https://deno.land/x/date_fns@v2.22.1/formatDistanceT
 import { DownloadParams, DownloadStatus } from "./types.d.ts";
 
 export class DLWP {
+  /** unique identifier */
   private _uid: string;
+  /** starting time */
   private _startTime: number;
-  private _cancel: boolean;
-  private _currentProgress: number;
+  /** triggered cancel */
+  private _cancel = false;
+  /** downloaded bytes */
+  private _currentProgress = 0;
+  /** total bytes to download */
   private _totalLength: number | "Unknown";
-  private _file: Deno.FsFile;
+  private _file = {} as Deno.FsFile;
+  /** progress callback timer */
+  private _progressTimer = 0;
+  /** speed and eta related timer */
+  private _speedTimer = 0;
+  /** prev bytes snap to calculate speed */
+  private _prevBytes = 0;
+  /** average speed in last 5 secs (in bytes) */
+  private _speed = 0;
+  /** File path */
+  private _path = "";
+  /** File name */
+  private _fileName = "";
 
-  /** Ignore */
+  /** Deno DLWP */
   constructor() {
     this._uid = crypto.randomUUID().split("-").pop()!;
     this._startTime = Date.now();
     this._cancel = false;
-    this._currentProgress = 0;
     this._totalLength = "Unknown";
-    this._file = {} as Deno.FsFile;
+  }
+
+  /** UID */
+  get id() {
+    return this._uid;
+  }
+
+  /** Cleanup function when  */
+  private async _cleanup(deleteFile = true) {
+    if (this._file.rid) {
+      Deno.close(this._file.rid);
+    }
+    if (deleteFile) {
+      await Deno.remove(this._path, { recursive: true });
+    }
+    if (this._progressTimer) {
+      clearInterval(this._progressTimer);
+    }
+    if (this._speedTimer) {
+      clearInterval(this._speedTimer);
+    }
   }
 
   /**
@@ -51,54 +87,57 @@ export class DLWP {
     this._currentProgress = 0;
 
     const parsed = parse(decodeURIComponent(url));
-    let [fileName] = parsed.base.split("?");
-    let _path = join(dir || Deno.cwd(), fileName);
+    this._fileName = parsed.base.split("?")[0];
+    this._path = join(dir || Deno.cwd(), this._fileName);
 
     // Delete existing file if overwrite set to true
     if (overwrite) {
-      await Deno.remove(_path, { recursive: true }).catch(() => {});
+      await Deno.remove(this._path, { recursive: true }).catch(() => {});
     }
 
     // If file exists, create a new file name
     try {
-      await Deno.lstat(_path);
-      fileName = this._uid + parsed.ext;
-      _path = join(dir || Deno.cwd(), fileName);
+      await Deno.lstat(this._path);
+      this._fileName = this._uid + parsed.ext;
+      this._path = join(dir || Deno.cwd(), this._fileName);
     } catch (_) {
       //
     }
 
-    this._file = await Deno.open(_path, {
+    this._file = await Deno.open(this._path, {
       create: true,
       read: true,
       append: true,
     });
 
-    const timer = setInterval(
+    this._progressTimer = setInterval(
       async () =>
         await onProgress(
           {
             current: this._currentProgress,
             total: this._totalLength,
+            speed: this._speed,
           },
           {
-            fileName,
-            path: _path,
+            fileName: this._fileName,
+            path: this._path,
           },
         ),
       delay,
     );
 
+    this._speedTimer = setInterval(() => {
+      // Average speed over last 5 seconds
+      this._speed = (this._currentProgress - this._prevBytes) / 5;
+      this._prevBytes = this._currentProgress;
+    }, 5000);
+
     await onStart();
 
     while (true) {
       if (this._cancel) {
-        if (this._file.rid) {
-          Deno.close(this._file.rid);
-          await Deno.remove(_path, { recursive: true });
-        }
+        await this._cleanup();
         await onCancel();
-        clearInterval(timer);
         break;
       }
 
@@ -110,11 +149,8 @@ export class DLWP {
       }
 
       if (done) {
-        if (this._file.rid) {
-          Deno.close(this._file.rid);
-        }
-        await onComplete({ fileName, path: _path });
-        clearInterval(timer);
+        await this._cleanup(false);
+        await onComplete({ fileName: this._fileName, path: this._path });
         break;
       }
     }
@@ -134,6 +170,7 @@ export class DLWP {
       totalLength: this._totalLength,
       timeDistanceInWords: formatDistance(this._startTime, {}),
       isDownloading: !this._cancel,
+      speed: this._speed,
     };
   }
 }
